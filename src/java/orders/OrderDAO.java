94669ab48634a08039475e78eaba0330098b0c4c
beu2901 <beu2901@gmail.com>
2025-04-18 12:05:51 +0700

    do sth in order dao

--- src/java/orders/OrderDAO.java
+++ src/java/orders/OrderDAO.java
@@ -11,8 +11,45 @@ import java.io.Serializable;
  *
  * @author beu29
  */
-public class OrderDAO implements Serializable{
-    
-    
-    
+public class OrderDAO implements Serializable {
+
+    public class OrdersDAO implements Serializable {
+
+        public int createNewOrder(String name, String address, String total)
+                throws SQLException, NamingException {
+            Connection con = null;
+            PreparedStatement stm = null;
+            ResultSet rs = null;
+
+            try {
+                con = DBHelpers.makeConnection();
+                if (con != null) {
+                    String sql = "Insert Into Orders(Name, Address, Total) "
+                            + "Output inserted.OrderID, inserted.Date "
+                            + "Values (?, ?, ?)";
+                    stm = con.prepareStatement(sql);
+                    stm.setString(1, name);
+                    stm.setString(2, address);
+                    stm.setBigDecimal(3, new BigDecimal(total));
+                    rs = stm.executeQuery();
+                    if (rs.next()) {
+                        int orderID = rs.getInt("OrderID");
+                        return orderID;
+                    }
+                } //end if con connect success
+            } finally {
+                if (rs != null) {
+                    rs.close();
+                }
+                if (stm != null) {
+                    stm.close();
+                }
+                if (con != null) {
+                    con.close();
+                }
+            }
+            return -1;
+        }
+
+    }
 }
