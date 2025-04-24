26038c8b00b15f0c79a23b911f6474b24d166f47
beu2901 <beu2901@gmail.com>
2025-04-24 07:44:57 +0700

    create application constant

---
+++ src/java/utils/ApplicationConstant.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils;
+
+/**
+ *
+ * @author beu29
+ */
+public class ApplicationConstant {
+
+    public class DispatchFeatures {
+
+        public static final String LOGIN_PAGE = "loginPage";
+        public static final String STARTUP_APP_CONTROLLER = "";
+        public static final String LOGIN_CONTROLLER = "loginAction";
+        public static final String LOGOUT_CONTROLLER = "logoutAction";
+        public static final String SIGN_UP_CONTROLLER = "signUpAction";
+
+        public static final String SEARCH_FULLNAME_CONTROLLER = "searchAction";
+        public static final String CONFIRM_DELETE_CONTROLLER = "confirmDeleteAction";
+        public static final String DELETE_ACCOUNT_CONTROLLER = "deleteAction";
+        public static final String UPDATE_ACCOUNT_CONTROLLER = "updateAction";
+
+        public static final String BUY_BOOK_CONTROLLER = "showBookAction";
+        public static final String ADD_BOOK_TO_CART_CONTROLLER = "addBookToCartAction";
+        public static final String VIEW_CART_PAGE = "viewCartPage";
+
+        public static final String REMOVE_BOOK_FROM_CART = "removeBookFromCartAction";
+        public static final String CHECK_OUT_ORDER_CONTROLLER = "checkOutAction";
+        public static final String CONFIRM_CHECK_OUT_CONTROLLER = "confirmCheckOutAction";
+    }
+
+}
