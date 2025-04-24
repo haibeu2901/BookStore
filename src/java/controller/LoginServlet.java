da6601ab017218f304e27d6e7973d9f752e4ddf9
beu2901 <beu2901@gmail.com>
2025-04-24 08:04:19 +0700

    modify login servlet

--- src/java/controller/LoginServlet.java
+++ src/java/controller/LoginServlet.java
@@ -7,10 +7,19 @@ package controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.naming.NamingException;
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import registration.RegistrationDAO;
+import registration.RegistrationDTO;
+import utils.ApplicationConstant;
 
 /**
  *
@@ -28,19 +37,52 @@ public class LoginServlet extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+            throws ServletException, IOException, SQLException, NamingException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet LoginServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet LoginServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+        PrintWriter out = response.getWriter();
+
+        String username = request.getParameter("txtUsername");
+        String password = request.getParameter("txtPassword");
+
+        String url = ApplicationConstant.LoginFeatures.INVALID;
+        try {
+            //call DAO
+            RegistrationDAO dao = new RegistrationDAO();
+            boolean result = dao.checkLogin(username, password);
+
+            if (result) {
+                HttpSession session = request.getSession();
+                //Khi login thành công => người dùng muốn dùng nhiều hơn 1 
+                //feature của ứng dụng => PHẢI tạo session mới để lưu trữ thông
+                //tin người dùng
+
+                String fullname = dao.showFullName(username);
+                boolean isAdmin = dao.checkAdmin(username);
+                RegistrationDTO dto = dao.showProfile(username);
+
+                session.setAttribute("USER", username);
+                session.setAttribute("FULL_NAME", fullname);
+
+                if (isAdmin) {
+                    session.setAttribute("ADMIN", isAdmin);
+                } else {
+                    session.setAttribute("ADMIN", isAdmin);
+                    session.setAttribute("SHOW_PROFILE", dto);
+                }
+
+                Cookie cookie = new Cookie(username, password);
+                cookie.setMaxAge(60 * 3);
+                response.addCookie(cookie);
+
+                url = ApplicationConstant.LoginFeatures.SEARCH_PAGE;
+            }//end if authentication is successful
+        } catch (NamingException ex) {
+            log("LoginServlet_Naming: " + ex.getMessage());
+        } catch (SQLException ex) {
+            log("LoginServlet_SQL: " + ex.getMessage());
+        } finally {
+            response.sendRedirect(url);
+            out.close();
         }
     }
 
@@ -56,7 +98,13 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(LoginServlet.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (NamingException ex) {
+            Logger.getLogger(LoginServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
@@ -70,7 +118,13 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(LoginServlet.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (NamingException ex) {
+            Logger.getLogger(LoginServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
